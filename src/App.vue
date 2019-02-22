844d046e16f4cba7ca80fdd1f89ec0c1e5ba7318
Chuma Umenze <donbona8@gmail.com>
2019-02-22 19:39:30 +0100

    Add navigation headers

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    preloader
+    site-header(:email="email", :sitePages="menuPages", :socialLinks="links")
+    router-view
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script lang="ts">
+  import { Component, Vue } from "vue-property-decorator";
+  import Preloader from "@/components/Preloader.vue";
+  import SiteHeader from "@/components/SiteHeader.vue";
+
+  @Component({
+    components: {
+      Preloader,
+      SiteHeader
+    },
+  })
+  export default class Home extends Vue {
+    private email: string = "hello@chumaumenze.com";
+    private menuPages: object[] = [
+      {title: "About", url: "/about"},
+      {title: "Works", url: "/#"},
+      {title: "Writings", url: "/#"},
+      {title: "Interests", url: "/#"},
+    ];
+    private links: object[] = [
+      {name: "Linkedin", url: "https://linkedin.com/in/chumaumenze", iconName: "fa-linkedin"},
+      {name: "Twitter", url: "https://twitter.com/chumaumenze", iconName: "fa-twitter"},
+      {name: "Github", url: "https://github.com/chumaumenze", iconName: "fa-github"},
+      {name: "Medium", url: "https://medium.com/@chumaumenze", iconName: "fa-medium"},
+    ];
   }
-}
+</script>
+
+<style lang="scss">
+  body{
+    font-family: 'Montserrat', sans-serif;
+    padding-bottom: 170px;
+    font-weight: 500;
+  }
 </style>
