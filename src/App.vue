fe9a1b885985790e7bbe768bae868221cc89a738
Chuma Umenze <donbona8@gmail.com>
2019-02-23 19:23:08 +0100

    Update App data

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,46 @@
 <template lang="pug">
   #app
     preloader
-    site-header(:email="email", :sitePages="menuPages", :socialLinks="links")
+    site-header(:email="owner.email", :sitePages="menuPages", :socialLinks="links", :logo="siteTitle")
+    site-social(:socialLinks="links")
     router-view
+    site-footer(:socialLinks="links", :email="owner.email", :siteOwner="owner.fullName", :logo="siteTitle")
 </template>
 
 <script lang="ts">
   import { Component, Vue } from "vue-property-decorator";
   import Preloader from "@/components/Preloader.vue";
   import SiteHeader from "@/components/SiteHeader.vue";
+  import SiteSocial from "@/components/SiteSocial.vue";
+  import SiteFooter from "@/components/SiteFooter.vue";
+  import { MenuPageOptions, SocialLinkOptions, SiteOwnerOptions } from "@/interfaces";
 
   @Component({
     components: {
       Preloader,
-      SiteHeader
+      SiteHeader,
+      SiteSocial,
+      SiteFooter
     },
   })
   export default class Home extends Vue {
-    private email: string = "hello@chumaumenze.com";
-    private menuPages: object[] = [
+    private siteTitle: string = "CHUMAUMENZE 🇳🇬";
+    private owner: SiteOwnerOptions = {
+      email: "hello@chumaumenze.com",
+      fullName: "Chuma Umenze",
+      username: "chumaumenze"
+    };
+    private menuPages: MenuPageOptions[] = [
       {title: "About", url: "/about"},
       {title: "Works", url: "/#"},
       {title: "Writings", url: "/#"},
       {title: "Interests", url: "/#"},
     ];
-    private links: object[] = [
+    private links: SocialLinkOptions[] = [
+      {name: "Github", url: "https://github.com/chumaumenze", iconName: "fa-github"},
+      {name: "Gitlab", url: "https://gitlab.com/chumaumenze", iconName: "fa-gitlab"},
       {name: "Linkedin", url: "https://linkedin.com/in/chumaumenze", iconName: "fa-linkedin"},
       {name: "Twitter", url: "https://twitter.com/chumaumenze", iconName: "fa-twitter"},
-      {name: "Github", url: "https://github.com/chumaumenze", iconName: "fa-github"},
       {name: "Medium", url: "https://medium.com/@chumaumenze", iconName: "fa-medium"},
     ];
   }
