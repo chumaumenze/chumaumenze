c9a24205c554845d40697d1a0838fdefab3812f8
Chuma Umenze <hello@chumaumenze.com>
2019-06-24 19:17:19 +0100

    Add Beta View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  .is-preload
+    #wrapper
+      #bg
+      #overlay
+      #main
+        // Header
+        header#header
+          h1 Chuma Umenze
+          p Software Engineer  •  Photographer  •  Iridescent
+          nav
+            ul
+              li
+                a.icon.brands.fa-twitter(href='https://twitter.com/chumaumenze')
+                  span.label Twitter
+              li
+                a.icon.brands.fa-linkedin-in(href='https://linkedin.com/in/chumaumenze')
+                  span.label LinkedIn
+              // li
+                a.icon.brands.fa-facebook-f(href='#')
+                  span.label Facebook
+              // li
+                a.icon.brands.fa-dribbble(href='#')
+                  span.label Dribbble
+              li
+                a.icon.brands.fa-github(href='https://github.com/chumaumenze')
+                  span.label Github
+              li
+                a.icon.solid.fa-envelope(href='mailto:hello@chumaumenze.com')
+                  span.label Email
+        // Footer
+        footer#footer
+          span.copyright
+            | © {{ year }} Chuma Umenze
+            a.attribute(href='http://html5up.net' target="blank" ref="no-opener") HTML5 UP
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import {Vue, Component} from 'vue-property-decorator';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    @Component
+    export default class Aerial extends Vue {
+        private year: number = 2019;
+        private mounted() {
+            window.onload = () => document.body.classList.remove('is-preload');
+            window.ontouchmove = () => false;
+            window.onorientationchange = () => document.body.scrollTop = 0;
+        }
+    }
 </script>
+
+<style src="@/assets/css/main.css"></style>
