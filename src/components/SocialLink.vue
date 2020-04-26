114fde9fb339b4fa0143a84e22ec5dba749e8178
Chuma Umenze <hello@chumaumenze.com>
2020-04-26 22:44:23 +0100

    Add Connect page

---
+++ src/components/SocialLink.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  div#blog-social-links
+    ul.social
+      li
+        a(href='http://medium.com/@chumaumenze', target='_blank', title='Medium')
+          svg(viewBox='0 0 16 16', xmlns='http://www.w3.org/2000/svg', fill-rule='evenodd', clip-rule='evenodd', stroke-linejoin='round', stroke-miterlimit='1.414')
+            path(d='M11.824 12.628l-.276.45.798.398 2.744 1.372c.15.076.294.11.418.11.278 0 .467-.177.467-.492V5.883l-4.15 6.745zm4.096-8.67c-.004-.003 0-.01-.003-.012l-4.825-2.412c-.06-.03-.123-.038-.187-.044-.016 0-.03-.01-.047-.01-.184 0-.368.092-.467.254l-.24.39-.5.814-1.89 3.08 1.89 3.076.5.813.5.812.59.95 4.71-7.64c.02-.03.01-.06-.02-.08zm-6.27 7.045L7.17 6.97l-.295-.477-.294-.477-.25-.416v4.867l3.32 1.663.5.25.5.25-.5-.813-.5-.813zM.737 1.68L.59 1.608c-.085-.042-.166-.062-.24-.062-.206 0-.35.16-.35.427v10.162c0 .272.2.594.442.716l4.145 2.08c.107.06.208.08.3.08.257 0 .438-.2.438-.53V4.01c0-.02-.012-.04-.03-.047L.738 1.68z')
+      li
+        a(href='http://twitter.com/chumaumenze', target='_blank', title='Twitter')
+          svg(viewBox='0 0 16 16', xmlns='http://www.w3.org/2000/svg', fill-rule='evenodd', clip-rule='evenodd', stroke-linejoin='round', stroke-miterlimit='1.414')
+            path(d='M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.37-1.337.64-2.085.79-.598-.64-1.45-1.04-2.396-1.04-1.812 0-3.282 1.47-3.282 3.28 0 .26.03.51.085.75-2.728-.13-5.147-1.44-6.766-3.42C.83 2.58.67 3.14.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.41-.02-.61-.058.42 1.304 1.63 2.253 3.07 2.28-1.12.88-2.54 1.404-4.07 1.404-.26 0-.52-.015-.78-.045 1.46.93 3.18 1.474 5.04 1.474 6.04 0 9.34-5 9.34-9.33 0-.14 0-.28-.01-.42.64-.46 1.2-1.04 1.64-1.7z', fill-rule='nonzero')
+      li
+        a(href='https://open.spotify.com/user/befjaofs7n7aa28s0x60cl1gk', target='_blank', title='Spotify')
+          svg(viewBox='0 0 16 16', xmlns='http://www.w3.org/2000/svg', fill-rule='evenodd', clip-rule='evenodd', stroke-linejoin='round', stroke-miterlimit='1.414')
+            path(d='M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.56-8-8-8zm3.68 11.56c-.16.24-.44.32-.68.16-1.88-1.16-4.24-1.4-7.04-.76-.28.08-.52-.12-.6-.36-.08-.28.12-.52.36-.6 3.04-.68 5.68-.4 7.76.88.28.12.32.44.2.68zm.96-2.2c-.2.28-.56.4-.84.2-2.16-1.32-5.44-1.72-7.96-.92-.32.08-.68-.08-.76-.4-.08-.32.08-.68.4-.76 2.92-.88 6.52-.44 9 1.08.24.12.36.52.16.8zm.08-2.24C10.16 5.6 5.88 5.44 3.44 6.2c-.4.12-.8-.12-.92-.48-.12-.4.12-.8.48-.92 2.84-.84 7.52-.68 10.48 1.08.36.2.48.68.28 1.04-.2.28-.68.4-1.04.2z', fill-rule='nonzero')
+
+</template>
+
+<script>
+  export default {
+    name: "SocialLink"
+  }
+</script>
+
+<style lang="scss" scoped>
+#blog-social-links {
+  ul {
+
+    &.social {
+      list-style-type: none;
+      margin: 1.5em 0 .6em;
+
+      li {
+        display: inline-block;
+        margin: 0 20px 0 0;
+
+        svg {
+          width: 24px;
+          fill: #8a8a8a;
+          -webkit-transition: opacity .3s ease;
+          -o-transition: opacity .3s ease;
+          transition: opacity .3s ease;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/Connect.vue
+++ src/pages/Connect.vue
@@ -1,17 +1,56 @@
 <template lang="pug">
   blog-layout(v-bind:stickyNavbar="stickyNavbar")
+    #blog-connect
+      h1 Drop a line...
+      p.subtitle Write me a message or connect with me on the social platforms.
+      .row
+        .eight.columns
+          div
+            form(name="Connect" data-netlify="true" method="POST" 
+              netlify-honeypot="honeypot")
+              .row
+                .six.columns
+                  label(for='blog-connect-form-name') Name
+                  input#blog-connect-form-name.u-full-width(type='text', 
+                    placeholder='Enter your name' name="Name")
+                .six.columns
+                  label(for='blog-connect-form-email') Email
+                  input#blog-connect-form-email.u-full-width(type='email', 
+                    placeholder='myusername@email.com' name="Email")
+              input#blog-connect-form-honeypot(name="honeypot" type="text")
+              label(for='blog-connect-form-message') Message
+              textarea#blog-connect-form-message.u-full-width(
+                placeholder='Hi Zuko …' name="Message")
+              //div.h-captcha(:data-sitekey="$config.webTags.hCaptcha.siteKey" 
+              //  :data-callback="$config.webTags.hCaptcha.callbackName")
+              input.button(type='submit', value='Send')
+
+        .four.columns
+          a(:href="`mailto:${this.$config.email}`") {{this.$config.email}}
+          social-link
+    hr
+
 </template>
 
 <style scoped>
+  #blog-connect {
+    margin-top: 6.5em;
+  }
+  
+  #blog-connect-form-honeypot {
+    display: none;
+  }
 
 </style>
 
 <script>
+  import SocialLink from "../components/SocialLink";
   export default {
     name: 'Contact',
+    components: {SocialLink},
     data() {
       return {
-        stickyNavbar: false
+        stickyNavbar: true
       }
     }
   }
