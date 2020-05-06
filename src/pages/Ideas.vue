29092d8739fa9f59fc9f90e0bd5818072099ff11
Chuma Umenze <hello@chumaumenze.com>
2020-05-06 02:30:56 +0100

    Add template for ideas

---
+++ src/pages/Ideas.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  blog-layout(stickyNavbar)
+    div#blog-ideas.page-mt
+      p Work in progress...
+</template>
+
+<script>
+  import GraphMeta from "~/mixins/GraphMeta.vue";
+
+  export default {
+    name: "Ideas",
+    mixins: [GraphMeta],
+    components: {},
+    data() {
+      return {
+        graphMeta: {
+          title: 'Ideas',
+          description: `Ideas`
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
